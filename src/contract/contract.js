0edc6ce85ba21a457d2bf602d71c9939066aa1bc
angel paderne <apcozar@gmail.com>
2024-12-27 10:22:49 +0100

    update from https to websockets

--- src/contract/contract.js
+++ src/contract/contract.js
@@ -1,9 +1,10 @@
+import { WebSocketProvider } from 'ethers';
 import { healthHubABI } from '../../abis/HealthHubABI.js';
-import { JsonRpcProvider, ethers  } from 'ethers';
+import { ethers  } from 'ethers';
 
-const CONTRACT_ADDRESS = '0x899F7b9ddab3924d67F4571B4155C2D261fCaF54';
+const CONTRACT_ADDRESS = '0xB515202ebAd40D6171097283C7A9eaA92fD89597';
 const CONTRACT_ABI = healthHubABI;
-const provider = new JsonRpcProvider('https://sepolia.infura.io/v3/82f0375fcd6844948892bf0305a6ba2a');
+const provider = new WebSocketProvider('wss://sepolia.infura.io/ws/v3/82f0375fcd6844948892bf0305a6ba2a');
 
 export default async function getContract() {
     return new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider);
